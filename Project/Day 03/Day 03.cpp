1e30901878df6fa167c6843ea0fb97b6b8c43aa5
Joan Reig <joanreigbergua@gmail.com>
2022-12-04 18:10:15 +0100

    Day 03 [Part 02]

--- Project/Day 03/Day 03.cpp
+++ Project/Day 03/Day 03.cpp
@@ -1,9 +1,16 @@
 // GIVEN the Day 03 [Explanation](https://adventofcode.com/2022/day/3) this is the implementation by REgiSTerKZz
 
 /*
+* PART 1
 *	Given a String
 *	Divide the string in half	(the string is always even in length)
 *	Check now the char in both half-strings that is the same
+*	Once found, check it's value depending on the conditions -> (lowercase 1 - 26, uppercase 27 - 52)
+*	Add them all
+* 
+* PART 2
+*	GIVEN group of 3 Strings
+*	Check what char have all 3 in common
 *	Once found, check it's value depending on the conditions -> (lowercase 1 - 26, uppercase 27 - 52)
 *	Add them all
 */
@@ -47,7 +54,6 @@ void PartOne(std::string _input) {
 	bool found = false;
 	char c = NULL;
 	int it = 0;
-	auto subs = 0;
 
 	// Split the input
 	halfs[0] = _input.substr(0, (_input.size() / 2));
@@ -72,15 +78,83 @@ void PartOne(std::string _input) {
 	totalScore += CalculateScore(c);
 }
 
+void PartTwo(std::vector<std::string> _input) {
+	bool found = false;
+	char c = NULL;
+	int it = 0;
+	int i1, i2, i3;
+	size_t sSize;
+
+	// Get smallest string size
+	sSize = std::min(std::min(_input[0].size(), _input[1].size()), _input[2].size());
+	
+	// Get smallest string
+	if (_input[0].size() == sSize)		// If _input 0 is smallest
+	{
+		i1 = 0;
+		i2 = 1;
+		i3 = 2;
+	}
+	else if (_input[1].size() == sSize)	// If _input 1 is smallest
+	{
+		i1 = 1;
+		i2 = 0;
+		i3 = 2;
+	}
+	else								// If _input 2 is smallest
+	{
+		i1 = 2;
+		i2 = 1;
+		i3 = 0;
+	}
+
+
+
+	// Find the char in common
+	while (!found)
+	{
+		size_t pos1 = _input[i2].find(_input[i1][it]);
+		size_t pos2 = _input[i3].find(_input[i1][it]);
+
+		if (pos1 != std::string::npos && pos2 != std::string::npos) {
+			c = _input[i2][pos1];
+			found = true;
+		}
+
+		if (it >= sSize-1)	{ found = true; }
+		it++;
+	}
+
+	// Calculate Score
+	totalScore += CalculateScore(c);
+}
+
 void ReadFile(std::string _file) {
 	std::string mStr;
 	std::ifstream mFile;
 
+	std::vector<std::string> mGroup = {"","",""};
+
 	mFile.open(_file);
 
+	// PART ONE
+	/*
 	while (getline(mFile, mStr))
 	{
 		PartOne(mStr);
+	}
+	*/
+
+	// PART TWO
+	while (true)
+	{
+		getline(mFile, mGroup[0]);
+		getline(mFile, mGroup[1]);
+		getline(mFile, mGroup[2]);
+
+		PartTwo(mGroup);
+
+		if (mFile.eof()) { break; }
 	}
 
 	mFile.close();
