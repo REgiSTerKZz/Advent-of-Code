878a5b794e5f5ebd1f2831fb5ea5df14f3f32e2c
Joan Reig <joanreigbergua@gmail.com>
2022-12-05 16:38:50 +0100

    Day 04 [Part 02]

--- Project/Day 04/Day 04.cpp
+++ Project/Day 04/Day 04.cpp
@@ -22,11 +22,6 @@ int totalSum = 0;
 
 /// Result: 464
 void PartOne(std::string _input) {
-
-	if (_input == "11-98,6-9") {
-		std::cout << "HERE\n";
-	}
-
 	std::vector<std::string> pairs = { "","" };
 	std::vector<int> intPairs(4);
 
@@ -62,6 +57,30 @@ void PartOne(std::string _input) {
 	}
 }
 
+/// Result: 770
+void PartTwo(std::string _input) {
+	std::vector<std::string> pairs = { "","" };
+	std::vector<int> intPairs(4);
+
+	// Split by commas
+	pairs[0] = _input.substr(0, _input.find(','));
+	pairs[1] = _input.substr(_input.find(',') + 1);		// +1 since we don't want the "comma"
+
+	// Split by dash
+	intPairs[0] = std::stoi(pairs[0].substr(0, pairs[0].find('-')));
+	intPairs[1] = std::stoi(pairs[0].substr(pairs[0].find('-') + 1));
+
+	intPairs[2] = std::stoi(pairs[1].substr(0, pairs[1].find('-')));
+	intPairs[3] = std::stoi(pairs[1].substr(pairs[1].find('-') + 1));
+
+
+	// Get rid of the one that don't even collide
+	if (intPairs[1] < intPairs[2] || intPairs[3] < intPairs[0]) { return; }
+
+	totalSum++;
+	std::cout << _input << "Overlap! \n";
+}
+
 void ReadFile(std::string _file) {
 	std::string mStr;
 	std::ifstream mFile;
@@ -73,7 +92,7 @@ void ReadFile(std::string _file) {
 	{
 		getline(mFile, mStr);
 
-		PartOne(mStr);
+		PartTwo(mStr);
 
 		if (mFile.eof()) { break; }
 	}
